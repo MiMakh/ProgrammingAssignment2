ef555bee9a840a55e679b15a2f2c0678852f5644
Mikhail Makhyanov <mmakhyanov@Mikhails-MacBook-Air.local>
2015-06-21 23:00:03 +0300

    initial commit

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,67 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
+
+
+## Solve for an inverse matrix of a given matrix, however firstly it checks whether it was computed before
+## First it sets the given matrix in universe, then it gets it, then it sets its mean and gets the mean
+
+
+##This part returns list of functions to set and get values of matrix and its mean
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv<-NULL
+  
+  ## sets the matrix
+  setz<-function(y){
+    x<<-y
+    inv<<-NULL
+  }
+  ##gets the matrix
+  getz<-function()x
+  
+  ##sets the inverse
+  setzinv<-function(solve) inv <<-solve
+  
+  ##gets the inverse
+  getzinv<-function() inv
+  
+  #returns functions above
+  list(setz=setz, getz=getz, setzinv=setzinv, getzinv=getzinv)
+  
 }
 
 
-## Write a short comment describing this function
+## Here we compute the inverse matrix, however check first whether it was computed before, if it was ->
+## -> reuturn cached value. If it wasn't then compute the inverse
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Firstly, check if it was computed
+  inv<-x$getzinv()
+  
+  if (!is.null(inv)) {
+    message("Getting Cached Data")
+    ## returns cached matrix
+    return(inv)
+  }
+  
+  ## If inverse wasn't calculated we compute it here
+  v <- x$getz()
+  inv <- solve(v)
+  
+  # After computation we cache it
+  x$setzinv(inv)
+  
+  #returns inverse matrix
+  inv
+  
 }
+
+##Sample input
+
+x<-matrix(1:4,2,2)
+x1<-makeCacheMatrix(x)
+#computing first time
+cacheSolve(x1)
+#computing second time
+cacheSolve(x1)
+#checking if the answer is right
+solve(x)
